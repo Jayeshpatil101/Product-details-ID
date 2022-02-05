eb1e007e2b5811bb85ca775981cb7c19e95c147f
Jayesh <Jayesh@DESKTOP-GOEFFQN>
2022-02-05 15:45:30 +0530

    Product details using the product id

---
+++ Product details using the product Id/src/main/java/Retrive.java
@@ -0,0 +1,71 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/Retrive")
+public class Retrive extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Retrive() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+			response.getWriter().append("Served at: ").append(request.getContextPath());
+		}
+
+		/**
+		 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+		 */
+		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			 PrintWriter pw=response.getWriter();
+			 ProductBean pb=new ProductBean();
+			 pb.setPid(Integer.parseInt(request.getParameter("pid")));
+	  		 pw.println("<html><body>");  
+	  		 response.setContentType("text/html");
+	  		
+	    	 try {
+	       		 Connection con=Dbconnection.dbConn();
+	       		 String sql="select * from product where pid=(?)";
+	    		 PreparedStatement ps=con.prepareStatement(sql);
+	    		 ps.setInt(1,pb.getPid());
+	    		 ResultSet rs=ps.executeQuery();
+	    		 RequestDispatcher rd=request.getRequestDispatcher("index.html");
+	    		 rd.include(request, response);
+	       		// pw.print("<br><center><h3>Pr</h3><br></center>");
+	      		
+	       		 pw.println("<center><table border=1 width=14% height=10%>");  
+	             pw.println("<tr><th>Product Id</th><th>Product Name</th><th>Price</th>"); 
+	       		 while(rs.next()) {
+	       			
+	       			 //pw.println(rs.getInt(1)+"   "+rs.getString(2));
+	       			 pw.println("<tr><td>" + rs.getInt(1)+ "</td><td>" + rs.getString(2) +"</td><td>"+ rs.getDouble(3)+"</td></tr></center>");   
+	       			 
+	       		 }
+	       		 pw.println("</table>");  
+	       		 pw.println("</html></body>");  
+	       		 //rd.include(request, response);
+	       		 
+	       	 } catch (ClassNotFoundException e) {
+	       		 // TODO Auto-generated catch block
+	       		 e.printStackTrace();
+	       	 } catch (SQLException e) {
+	       		 // TODO Auto-generated catch block
+	       		 e.printStackTrace();
+	       	 }	 
+	    }
+
+	}
